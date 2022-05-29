c0cd1164806d40ad51ba80170f01a80a097bcfd4
Oriol Angelet <uriaeri@gmail.com>
2022-05-29 17:34:47 +0200

    initial commit

---
+++ Project31/Project31/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Uri on 29/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
